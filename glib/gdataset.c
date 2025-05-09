fuhsnn/glib

commit f6a9d0479622593c8339346b7298ca04f31a56be

GDataSet: silently accept NULL/0 as keys
This used to be the behaviour before we made these functions
threadsafe; keep it that way.

https://bugzilla.gnome.org/show_bug.cgi?id=737741

glib/gdataset.c
@@ -850,7 +850,6 @@ g_datalist_id_dup_data (GData          **datalist,
   GDataElt *data, *data_end;
 
   g_return_val_if_fail (datalist != NULL, NULL);
-  g_return_val_if_fail (key_id != 0, NULL);
 
   g_datalist_lock (datalist);
 
@@ -1029,7 +1028,7 @@ g_datalist_get_data (GData	 **datalist,
       data_end = data + d->len;
       while (data < data_end)
 	{
-	  if (strcmp (g_quark_to_string (data->key), key) == 0)
+	  if (g_strcmp0 (g_quark_to_string (data->key), key) == 0)
 	    {
 	      res = data->data;
 	      break;

commit 1bca341967c7b7bf5e07b9bdcc50e92cd743393d

gdataset: correctly handle NULL string in g_datalist_get_data()
All code that adds an entry to GData calls down to datalist_append().
No caller would never pass a zero key_id. So a GData will never contain
the zero GQuark.

g_datalist_get_data() allows to lookup the keys by string. Looking up
by string, only makes sense if the string is a valid GQuark. And the
NULL string is not a valid GQuark (or, in another interpretation is to
say that the GQuark of NULL is zero). In any case, the GData does not
contain the zero quark and it should never find a NULL string there.

Note however that you can add invalid (non-zero) GQuarks to GData. That
is because we avoid the overhead of checking that the non-zero key_is is
infact a valid GQuark. Thus, if you called

  g_datalist_get_data(&data, NULL)

the check

  if (g_strcmp0 (g_quark_to_string (data_elt->key), key) == 0)

would have found entries where the key is not a valid GQuark.

Fix that. Looking up a NULL string should never finds an entry.

Fixes: f6a9d0479622 ('GDataSet: silently accept NULL/0 as keys')

glib/gdataset.c
@@ -1429,6 +1429,9 @@ g_datalist_get_data (GData **datalist,
 
   g_return_val_if_fail (datalist != NULL, NULL);
 
+  if (G_UNLIKELY (!key))
+    return NULL;
+
   d = g_datalist_lock_and_get (datalist);
 
   if (!d)
@@ -1442,6 +1445,8 @@ g_datalist_get_data (GData **datalist,
 
       for (i = 0; i < d->len; i++)
         {
+          const char *qstr;
+
           data_elt = &d->data[i];
           /* Here we intentionally compare by strings, instead of calling
            * g_quark_try_string() first.
@@ -1449,7 +1454,8 @@ g_datalist_get_data (GData **datalist,
            * See commit 1cceda49b60b ('Make g_datalist_get_data not look up the
            * quark').
            */
-          if (g_strcmp0 (g_quark_to_string (data_elt->key), key) == 0)
+          qstr = g_quark_to_string (data_elt->key);
+          if (qstr && strcmp (qstr, key) == 0)
             {
               res = data_elt->data;
               goto out;
@@ -1459,7 +1465,7 @@ g_datalist_get_data (GData **datalist,
     }
 
   key_id = g_quark_try_string (key);
-  if (key_id == 0 && key)
+  if (key_id == 0)
     goto out;
 
   data_elt = g_hash_table_lookup (index, &key_id);

glib/tests/dataset.c
@@ -254,9 +254,6 @@ test_datalist_basic (gconstpointer test_data)
   ret = g_datalist_get_data (&list, "two");
   g_assert_null (ret);
 
-  /* Despite the bug below, we won't find a NULL entry at this point.
-   * If !HAS_MANY, there are no bogus quarks and we don't find them.
-   * With HAS_MANY, we have bogus quarks but use the lookup index. */
   ret = g_datalist_get_data (&list, NULL);
   g_assert_null (ret);
 
@@ -268,13 +265,7 @@ test_datalist_basic (gconstpointer test_data)
 
   /* Ensure that we don't find the BOGUS_QUARK when looking up by NULL. */
   ret = g_datalist_get_data (&list, NULL);
-  if (HAS_MANY)
-    g_assert_null (ret);
-  else
-    {
-      /* This is a bug. */
-      g_assert_true (ret == data);
-    }
+  g_assert_null (ret);
 
   g_datalist_clear (&list);
 }